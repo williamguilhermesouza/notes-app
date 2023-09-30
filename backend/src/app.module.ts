2bce1d29ed3e888316b9cf8319b1d0084f533fc0
williamguilhermesouza <williamguilhermesouza@gmail.com>
2023-09-30 14:47:55 -0300

    completed setup of db, next step models

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NoteModule } from './note/note.module';
 
 @Module({
-  imports: [NoteModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'corelabbr',
+      entities: [],
+      synchronize: true,
+    }),
+    NoteModule],
   controllers: [AppController],
   providers: [AppService],
 })
