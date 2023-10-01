fcb243d61c5e6e14c889a445bf683696a7812d2d
williamguilhermesouza <williamguilhermesouza@gmail.com>
2023-10-01 01:37:50 -0300

    added bootstrap

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,11 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
 import NotesSlider from '@/components/NotesSlider';
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
@@ -17,8 +14,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <div className="top-area">
-          <input type="text" placeholder='Create a note' />
+        <div className='container text-center'>
+          <div className="row">
+            <input className='col-12' type="text" placeholder='Create a note' />
+          </div>
         </div>
         <div className="middle-area">
           <NotesSlider/>
