fcf133fa609b4ded16ec39e1e4075975f2733bf1
williamguilhermesouza <williamguilhermesouza@gmail.com>
2023-10-01 12:10:31 -0300

    working on css notes cards

--- frontend/src/components/Note/index.tsx
+++ frontend/src/components/Note/index.tsx
@@ -1,15 +1,32 @@
+import styles from './styles.module.css';
+import Image from 'next/image';
+import 'bootstrap/dist/css/bootstrap.css';
+import fav from '@/../public/fav.ico';
+import notfav from '@/../public/notfav.ico';
+
 
 export default function Note() {
     return(
         //need to create color style
-        <div className='border rounded d-inline'>
-            <h3>Note name</h3>
-            <a href=""><img src="" alt="favorite-btn" /></a>
-            <ul>
-                <li>note 1</li>
-                <li>note 2</li>
-                <li>note 3</li>
-            </ul>
+        <div className={`${styles.noteCard} card border rounded`}>
+            <div className="card-body">
+                <div className={styles.cardHeader}>
+                    <h5 className="card-title">Note name</h5>
+                    <Image
+                        src={fav}
+                        width={20}
+                        height={20}
+                        alt="favorite"
+                    />
+                </div>
+                <div className="card-text">
+                    <ul>
+                        <li>note 1</li>
+                        <li>note 2</li>
+                        <li>note 3</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 }
--- frontend/src/components/NotesSlider/index.tsx
+++ frontend/src/components/NotesSlider/index.tsx
@@ -1,10 +1,12 @@
 import Note from "../Note";
 import 'bootstrap/dist/css/bootstrap.css';
+import styles from './styles.module.css';
+
 
 export default function NotesSlider() {
     return(
-        <div className="container mt-3 w-80">
-            <div className="row text-center">
+        <div className={"container mt-3 w-80"}>
+            <div className={`${styles.mainRow} row text-center`}>
                 <Note/>
                 <Note/>
                 <Note/>
