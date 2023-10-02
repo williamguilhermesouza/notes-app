dc0125b4374e204e5f8a8cafff310ea589111d93
williamguilhermesouza <williamguilhermesouza@gmail.com>
2023-10-02 00:16:34 -0300

    removed mockups

--- frontend/src/components/NotesSlider/index.tsx
+++ frontend/src/components/NotesSlider/index.tsx
@@ -5,48 +5,6 @@ import backend from "@/pages/services/backend";
 import { useState, useEffect } from "react";
 import { Modal, Button } from 'react-bootstrap';
 
-const NoteMock = {
-    "id": 0,
-    "name": "tarefas",
-    "creation_date": "2023-09-30T13:58:13.000Z",
-    "favorite": false,
-    "color": 1,
-    "content": [
-        "estudar para prova",
-        "passear com dogua"
-    ]
-}
-
-const NoteMock2 = {
-    "id": 1,
-    "name": "shows",
-    "creation_date": "2023-09-30T13:58:13.000Z",
-    "favorite": true,
-    "color": 1,
-    "content": [
-        "ir no show do blind guardian",
-        "ir no megadeath",
-        "ir no show do shaman",
-        "ir no show do birobiro",
-        "ir no show do acdc",
-    ]
-}
-
-const NoteMock3 = {
-    "id": 2,
-    "name": "compras",
-    "creation_date": "2023-09-30T13:58:13.000Z",
-    "favorite": false,
-    "color": 1,
-    "content": [
-        "leite",
-        "ovo",
-        "pao",
-        "manteiga",
-        "fumo de rolo"
-    ]
-}
-
 interface noteInterface {
     id: number;
     name: string;
@@ -55,9 +13,6 @@ interface noteInterface {
     color: number;
     content: string[];
 }
-
-
-const NoteArray = [NoteMock, NoteMock2, NoteMock3];
 
 export default function NotesSlider() {
     const [notes, setNotes] = useState([]);
